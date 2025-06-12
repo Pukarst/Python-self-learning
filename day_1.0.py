25cc68203c4aecf20b4387dbc7863d2513f9052e
pukar_bade <bade5pukar@gmail.com>
2025-06-12 18:46:58 +0545

    introduction

---
+++ day_1.0.py
@@ -0,0 +1,11 @@
+"""
+Python is the high level,interpreted programming language.
+It was created by Guido Van Rossum.
+It is the powerful and flexible language that is easy to learn and suitable for the begineers and the advance users.
+
+Features of Python
+It is simple and easy to understand.
+It provide the very big library support.
+It has the simple syntax similar to the English language.
+It can be treated in the procedural,object-oriented or the functional way.
+"""
