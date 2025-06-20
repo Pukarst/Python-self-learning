5d7ce354301b9f74f97a16953c3fd06b31993f85
pukar_bade <bade5pukar@gmail.com>
2025-06-20 08:32:44 +0545

    python numbers

---
+++ 5.py
@@ -0,0 +1,20 @@
+num=66
+a=int(num)
+b=float(num)
+c=complex(num)
+print(a)
+print(b)
+print(c)
+
+
+num=66e4
+d=int(num)
+e=float(num)
+f=complex(num)
+print(d)
+print(e)
+print(f)
+
+
+import random
+print('The random number generated is',random.randint(1,11))
