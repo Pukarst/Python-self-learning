9afb91e51d11d70a6308a7be13ce31fcc8bcf6c0
pukar_bade <bade5pukar@gmail.com>
2025-06-20 07:58:37 +0545

    python comment

---
+++ 2.py
@@ -0,0 +1,27 @@
+# This is the single line comment
+print('Python') # the comment will not be execute.
+
+
+#for multiple line comment
+
+# twinkle twinkle little star
+# how i wonder what you are
+# up above the world so high
+# like a diamond in the sky
+
+
+
+'''
+twinkle twinkle little star
+how i wonder what you are
+up above the world so high
+like a diamond in the sky
+''' 
+#        OR
+
+"""
+twinkle twinkle little star
+how i wonder what you are
+up above the world so high
+like a diamond in the sky
+"""
